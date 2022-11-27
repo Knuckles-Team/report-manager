4fedf2187e348973a9f7203ec45424ac9d822916
Audel Rouhi <knucklessg1@gmail.com>
2022-11-27 02:33:22 -0600

    Update setup.py
    Updated URL

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description=description,
     long_description=f'{readme}',
     long_description_content_type='text/markdown',
-    url='https://github.com/Knucklessg1/report-manager',
+    url='https://github.com/Knuckles-Team/report-manager',
     author=__author__,
     author_email='knucklessg1@gmail.com',
     license='Unlicense',
